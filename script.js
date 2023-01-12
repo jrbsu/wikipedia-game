e83b04258f97fec85aaa599d85b129886d47c41d
Joe Sutherland <jrbsutherland@gmail.com>
2023-01-11 16:54:31 -0800

    correctArray reset

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ function mainFunction() {
 		descURL = "";
 		summaries = ["","","","","","","","","",""];
 		images = ["","","","","","","","","",""];
+		correctArray = [0,1,2,3,4,5,6,7,8,9];
 		topTen = [];
 		lives = 3;
 		points = 0;
